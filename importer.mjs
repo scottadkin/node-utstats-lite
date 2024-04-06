ca439df855645c9511adb3a41199ae483a993f56
Scott Adkin <scottadkin001@gmail.com>
2024-04-06 13:34:47 +0100

    check encoding of file before import

--- importer.mjs
+++ importer.mjs
@@ -4,18 +4,49 @@ import Message from "./src/app/lib/message.mjs";
 //const config = require('./config.json');
 import {simpleQuery} from "./src/app/lib/database.mjs";
 import { FTPImporter } from "./src/app/lib/ftpimporter.mjs";
-import { readFile } from 'node:fs/promises';
+import { readFile, readdir } from 'node:fs/promises';
 import { MatchParser } from "./src/app/lib/matchParser.mjs";
+import config from "./config.mjs";
+import Encoding from 'encoding-japanese';
 
 new Message('Node UTStats 2 Importer module started.','note');
 
-async function openFile(){
+async function parseLog(file){
 
-    const data = await readFile("Logs/test.txt", {"encoding": "utf16le"});
+    new Message(`Starting parsing of log ${file}`,"note");
+
+    const url = `${config.importedLogsFolder}/${file}`;
+
+    const dummy = await readFile(url);
+
+    let detectedEncoding = Encoding.detect(dummy);
+
+    if(detectedEncoding === "UTF16") detectedEncoding = "utf16le";
+
+    const data = await readFile(url, detectedEncoding);
 
     const m = new MatchParser(data);
 
     await m.main();
+}
+
+
+async function parseLogs(){
+
+    const files = await readdir(config.importedLogsFolder);
+    console.table(files);
+
+    for(let i = 0; i < files.length; i++){
+
+        console.log(i);
+
+        const f = files[i];
+        
+
+        if(!f.toLowerCase().startsWith(config.logFilePrefix)) continue;
+
+        await parseLog(f);
+    }
 }
 
 
@@ -36,9 +67,12 @@ async function openFile(){
         await test.connect();
 
     }
-    
-    await openFile();
 
+
+    
+   // await openFile();
+
+    await parseLogs();
 
     
     process.exit();
