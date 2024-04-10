5519d6af6955f27a8f4948f522d8b843e91b50a6
Scott Adkin <scottadkin001@gmail.com>
2024-04-10 21:33:10 +0100

    basic weapon kill/death table

--- src/app/UI/Match/WeaponStats.js
+++ src/app/UI/Match/WeaponStats.js
@@ -3,30 +3,7 @@ import Header from "../Header";
 import { ignore0, getPlayer, getTeamColorClass } from "@/app/lib/generic.mjs";
 import CountryFlag from "../CountryFlag";
 
-function getName(weapons, id){
-
-    if(weapons[id] !== undefined) return weapons[id];
-
-    return "Not Found";
-}
-
-export default function WeaponStats({data, totalTeams, players}){
-
-    const orderedNames = [];
-    
-    for(const [weaponId, weaponName] of Object.entries(data.names)){
-        orderedNames.push({"id": weaponId, "name": weaponName});
-    }
-
-    orderedNames.sort((a, b) =>{
-
-        a = a.name.toLowerCase();
-        b = b.name.toLowerCase();
-
-        if(a < b) return -1;
-        if(a > b) return 1;
-        return 0;
-    });
+function renderBasicTables(orderedNames, data, players, totalTeams){
 
     const tables = [];
 
@@ -37,16 +14,15 @@ export default function WeaponStats({data, totalTeams, players}){
         "teamKills": {"title": "Team Kills"},
     };
 
-
     for(let i = 0; i < orderedNames.length; i++){
 
         const weapon = orderedNames[i];
 
         const currentRows = [];
 
-        for(let x = 0; x < data.data.length; x++){
+        for(let x = 0; x < data.length; x++){
 
-            const d = data.data[x];
+            const d = data[x];
 
             if(d.weapon_id !== parseInt(weapon.id)) continue;
             
@@ -76,10 +52,109 @@ export default function WeaponStats({data, totalTeams, players}){
                 width={4}
             />);
         }
+    }
 
+
+    return tables;
+}
+
+function getPlayerWeaponStats(data, playerId, weaponId){
+
+    playerId = parseInt(playerId);
+    weaponId = parseInt(weaponId);
+
+    for(let i = 0; i < data.length; i++){
+
+        const d = data[i];
+
+        if(d.player_id !== playerId) continue;
+        if(d.weapon_id !== weaponId) continue;
+
+        return {"kills": d.kills, "deaths": d.deaths};
+    }
+
+    return null;
+}
+
+function renderKDTable(orderedNames, data, players, totalTeams){
+
+    const rows = [];
+
+    const headers = {
+        "player": {"title": "Player"}
+    };
+
+
+    for(let i = 0; i < orderedNames.length; i++){
+
+        const n = orderedNames[i];
+
+        headers[`weapon_${n.id}`] = {"title": n.name};
+    }
+
+    for(const [pId, player] of Object.entries(players)){
+
+        const playerId = parseInt(pId);
+
+        const columns = {};
+
+        columns.player = {
+            "value": player.name.toLowerCase(), 
+            "displayValue": <><CountryFlag code={player.country}/>{player.name}</>,
+            "className": `text-left ${(totalTeams < 2) ? "" : getTeamColorClass(player.team)}`
+        };
+
+        for(let i = 0; i < orderedNames.length; i++){
+
+            const n = orderedNames[i];
+
+            const weaponStats = getPlayerWeaponStats(data, playerId, n.id);
+
+            if(weaponStats === null){
+
+                columns[`weapon_${n.id}`] = {
+                    "value": "",
+                    "displayValue": <></>
+                };
+            }else{
+        
+                columns[`weapon_${n.id}`] = {
+                    "value": weaponStats.kills,
+                    "displayValue": <>{weaponStats.kills} - {weaponStats.deaths}</>
+                };
+            }
+    
+        }
+
+        rows.push(columns);
 
     }
 
+    return <InteractiveTable headers={headers} rows={rows}/>
+    
+}
+
+export default function WeaponStats({data, totalTeams, players}){
+
+    const orderedNames = [];
+    
+    for(const [weaponId, weaponName] of Object.entries(data.names)){
+        orderedNames.push({"id": weaponId, "name": weaponName});
+    }
+
+    orderedNames.sort((a, b) =>{
+
+        a = a.name.toLowerCase();
+        b = b.name.toLowerCase();
+
+        if(a < b) return -1;
+        if(a > b) return 1;
+        return 0;
+    });
+
+    let tables = renderKDTable(orderedNames, data.data, players, totalTeams);
+
+    
     return <>
         <Header>Weapon Stats</Header>
         {tables}
