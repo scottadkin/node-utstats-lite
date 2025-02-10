e86a2e222ec22d1123f428ffc0ed9102d84c2677
Scott Adkin <scottadkin001@gmail.com>
2025-02-10 16:05:27 +0000

    Split into team tables, ignore None

--- src/app/UI/Match/ClassicWeaponStats.js
+++ src/app/UI/Match/ClassicWeaponStats.js
@@ -6,7 +6,7 @@ import InteractiveTable from "../InteractiveTable";
 import { ignore0, getPlayer, getTeamColorClass } from "@/app/lib/generic.mjs";
 import PlayerLink from "../PlayerLink";
 
-function renderTable(selectedWeapon, data, players, totalTeams){
+function renderTable(selectedWeapon, data, players, targetTeam){
 
 
     const headers = {
@@ -19,6 +19,16 @@ function renderTable(selectedWeapon, data, players, totalTeams){
         "damage": {"title": "Damage"},
     };
 
+    const totals = {
+        "entries": 0,
+        "kills": 0,
+        "deaths": 0,
+        "shots": 0,
+        "hits": 0,
+        "accuracy": 0,
+        "damage": 0,
+    };
+
     const rows = [];
 
     for(let i = 0; i < data.length; i++){
@@ -29,10 +39,20 @@ function renderTable(selectedWeapon, data, players, totalTeams){
 
         const p = getPlayer(players, d.player_id);
 
+        if(targetTeam !== null && p.team !== targetTeam) continue;
+
+
+        totals.entries++;
+        totals.kills += d.kills;
+        totals.deaths += d.deaths;
+        totals.shots += d.shots;
+        totals.hits += d.hits;
+        totals.damage += d.damage;
+
         rows.push({
             "player": {"value": p.name.toLowerCase(),
                 "displayValue": <PlayerLink  id={d.player_id} country={p.country} bNewTab={true}>{p.name}</PlayerLink>, 
-            "className": `text-left ${(totalTeams > 1) ? getTeamColorClass(p.team) : ""}`},
+            "className": `player-name-td text-left ${(targetTeam !== null) ? getTeamColorClass(p.team) : ""}`},
             "kills": {"value": d.kills, "displayValue": ignore0(d.kills)},
             "deaths": {"value": d.deaths, "displayValue": ignore0(d.deaths)},
             "shots": {"value": d.shots, "displayValue": ignore0(d.shots)},
@@ -41,7 +61,41 @@ function renderTable(selectedWeapon, data, players, totalTeams){
             "damage": {"value": d.damage, "displayValue": ignore0(d.damage)},
         });
     }
+
+    if(rows.length === 0) return null;
+
+    if(rows.length > 1){
+        rows.push({
+            "bAlwaysLast": true,
+            "player": {"value": "",
+                "displayValue": "Totals", 
+            "className": `text-left team-none`},
+            "kills": {"value": totals.kills, "displayValue": ignore0(totals.kills)},
+            "deaths": {"value": totals.deaths, "displayValue": ignore0(totals.deaths)},
+            "shots": {"value": totals.shots, "displayValue": ignore0(totals.shots)},
+            "hits": {"value": totals.hits, "displayValue": ignore0(totals.hits)},
+            "acc": {"value": totals.accuracy, "displayValue": `${totals.accuracy.toFixed(2)}%`},
+            "damage": {"value": totals.damage, "displayValue": ignore0(totals.damage)},
+        });
+    }
+
     return <InteractiveTable width={3} headers={headers} rows={rows}/>
+}
+
+function renderTables(selectedWeapon, data, players, totalTeams){
+
+    const elems = [];
+
+    if(totalTeams < 2){
+        return renderTable(selectedWeapon, data, players, null);
+    }
+
+    for(let i = 0; i < totalTeams; i++){
+
+        elems.push(<div key={i}>{renderTable(selectedWeapon, data, players, i)}</div>);
+    }
+
+    return elems;
 }
 
 export default function ClassicWeaponStats({weaponNames, weaponImages, data, players, totalTeams, firstWeapon}){
@@ -55,7 +109,7 @@ export default function ClassicWeaponStats({weaponNames, weaponImages, data, pla
 
     for(const [value, name] of Object.entries(weaponNames)){
 
-        if(value === "0") continue;
+        if(value === "0" || name.toLowerCase() === "none") continue;
 
         tabOptions.push({name, value});
     }
@@ -77,6 +131,6 @@ export default function ClassicWeaponStats({weaponNames, weaponImages, data, pla
         <Tabs options={tabOptions} selectedValue={selectedWeapon} changeSelected={(value) =>{
             setSelectedWeapon(value);
         }}/>
-        {renderTable(selectedWeapon, data, players, totalTeams, setSelectedWeapon)}
+        {renderTables(selectedWeapon, data, players, totalTeams, setSelectedWeapon)}
     </>
 }
