938a48a47d2079641c43a01986c0aebd664c8124
Scott Adkin <scottadkin001@gmail.com>
2026-01-24 15:28:23 +0000

    users no longer need to click search button

--- public/js/ui/matches.js
+++ public/js/ui/matches.js
@@ -30,10 +30,10 @@ function renderMatchesTable(parent, data, bMapsPage){
 
     for(let i = 0; i < headers.length; i++){
 
-        headerRow.appendChild(UITableHeaderColumn({"content": headers[i]}));
+        headerRow.append(UITableHeaderColumn({"content": headers[i]}));
     }
 
-    table.appendChild(headerRow);
+    table.append(headerRow);
 
 
     for(let i = 0; i < matches.length; i++){
@@ -45,25 +45,25 @@ function renderMatchesTable(parent, data, bMapsPage){
         const url = `/match/${m.id}`;
 
         if(!bMapsPage){
-            row.appendChild(UITableColumn({"content": m.map_name, "className": "text-left", url}));
+            row.append(UITableColumn({"content": m.map_name, "className": "text-left", url}));
         }
-        row.appendChild(UITableColumn({"content": m.gametype_name, "className": "font-small", url}));
-        row.appendChild(UITableColumn({"content": m.server_name, "className": "font-small", url}));
-        row.appendChild(UITableColumn({"content": m.date, "parse": ["date"], "className": "playtime", url}));
-        row.appendChild(UITableColumn({"content": m.players, "parse": ["ignore0"], url}));
-        row.appendChild(UITableColumn({"content": m.playtime, "parse": ["playtime"], "className": "playtime", url}));
+        row.append(UITableColumn({"content": m.gametype_name, "className": "font-small", url}));
+        row.append(UITableColumn({"content": m.server_name, "className": "font-small", url}));
+        row.append(UITableColumn({"content": m.date, "parse": ["date"], "className": "playtime", url}));
+        row.append(UITableColumn({"content": m.players, "parse": ["ignore0"], url}));
+        row.append(UITableColumn({"content": m.playtime, "parse": ["playtime"], "className": "playtime", url}));
 
         const a = document.createElement("td");
         UIMatchScoreBox(a, m, true, false);
-        row.appendChild(a);
-        table.appendChild(row);
+        row.append(a);
+        table.append(row);
     }
 
 
-    wrapper.appendChild(table);
+    wrapper.append(table);
 
 
-    parent.appendChild(wrapper); 
+    parent.append(wrapper); 
 }
 
 class MatchRichViewBox{
@@ -76,7 +76,7 @@ class MatchRichViewBox{
         this.wrapper = document.createElement("a");
         this.wrapper.href = `/match/${data.id}`;
         this.wrapper.className = "rich-wrapper";
-        this.parent.appendChild(this.wrapper);
+        this.parent.append(this.wrapper);
 
         this.createElems()
         
@@ -92,34 +92,34 @@ class MatchRichViewBox{
         const gametype = document.createElement("div");
         gametype.className = "white";
 
-        gametype.appendChild(document.createTextNode(d.gametype_name));
-        info.appendChild(gametype);
+        gametype.append(document.createTextNode(d.gametype_name));
+        info.append(gametype);
 
-        info.appendChild(document.createTextNode(toDateString(d.date, true)));
+        info.append(document.createTextNode(toDateString(d.date, true)));
 
         const players = document.createElement("div");
         players.className = "white";
         players.innerHTML = `${d.players} Player${(d.players === 1) ? "" : "s"}`;
-        info.appendChild(players);
+        info.append(players);
 
-        info.appendChild(document.createTextNode(toPlaytime(d.playtime)));
+        info.append(document.createTextNode(toPlaytime(d.playtime)));
 
-        this.wrapper.appendChild(info);
+        this.wrapper.append(info);
     }
 
     createElems(){
 
         const title = document.createElement("div");
         title.className = "rich-title";
-        title.appendChild(document.createTextNode(this.data.map_name));
-        this.wrapper.appendChild(title);
+        title.append(document.createTextNode(this.data.map_name));
+        this.wrapper.append(title);
 
         const image = document.createElement("img");
         image.className = "rich-image";
         image.alt = "map-sshot";
         image.src = `/images/maps/${this.data.map_image}`;
 
-        this.wrapper.appendChild(image);
+        this.wrapper.append(image);
         this.createInfoElems();
         UIMatchScoreBox(this.wrapper, this.data, false, false);
     }
@@ -135,7 +135,7 @@ class MatchesRichView{
         this.wrapper = document.createElement("div");
         this.wrapper.className = "rich-outter t-width-1";
 
-        this.parent.appendChild(this.wrapper);
+        this.parent.append(this.wrapper);
 
 
         this.createElems();
@@ -154,4 +154,117 @@ class MatchesRichView{
             this.elems.push(new MatchRichViewBox(this.wrapper, d));
         }
     }
+}
+
+class MatchesSearchForm{
+
+    constructor(parent, servers, gametypes, maps, selectedServer, selectedGametype, selectedMap, selectedDisplayMode){
+
+        this.parent = document.querySelector(parent);
+
+        this.servers = servers;
+        this.gametypes = gametypes;
+        this.maps = maps;
+
+        this.selectedServer = selectedServer;
+        this.selectedGametype = selectedGametype;
+        this.selectedMap = selectedMap;
+        this.selectedDisplayMode = selectedDisplayMode;
+
+        UIHeader(this.parent, "Recent Matches");
+        this.wrapper = UIDiv("form");
+        
+        this.createFormElems();
+        this.parent.append(this.wrapper);
+    }
+
+    changeSelected(key, value){
+
+        this[key] = value;
+        const url = `/matches/?s=${this.selectedServer}&g=${this.selectedGametype}&m=${this.selectedMap}&display=${this.selectedDisplayMode}`;
+        window.location.href = url;
+    }
+
+    createSelect(type){
+
+        let title = "";
+        let selectedKey = "";
+        let targetNames = [];
+        let id = "";
+
+        if(type === "servers"){
+
+            selectedKey = "selectedServer";
+            title = "Server";
+            id = "s";
+            targetNames = this.servers;
+
+        }else if(type === "gametypes"){
+
+            selectedKey = "selectedGametype";
+            title = "Gametype";
+            id = "g";
+            targetNames = this.gametypes;
+
+        }else if(type === "maps"){
+
+            selectedKey = "selectedMap";
+            title = "Map";
+            id = "m";
+            targetNames = this.maps;
+
+        }else if(type === "display"){
+
+            selectedKey = "selectedDisplayMode";
+            title = "Display Mode";
+            id = "display";
+
+            targetNames = [
+                {"name": "Default View", "id": "default"},
+                {"name": "Table View", "id": "table"}
+            ];
+        }
+
+        if(type !== "display"){
+
+            targetNames.sort((a, b) =>{
+                a = a.name.toLowerCase();
+                b = b.name.toLowerCase();
+
+                if(a < b){
+                    return -1;
+                }else if(a > b){
+                    return 1;
+                }
+                return 0;
+            });
+        }
+
+        const row = UIDiv("form-row");
+        row.append(UILabel(title));
+
+        const select = new UISelect(
+            row, 
+            targetNames.map((s) =>{
+                return {"display": s.name, "value": s.id};
+            }), 
+            this[selectedKey],
+            (newTab) => { 
+                this.changeSelected(selectedKey, newTab);
+            }
+        );
+
+        select.select.id = select.select.name = id;
+
+        return row;
+    }
+
+    createFormElems(){
+
+        this.wrapper.append(this.createSelect("servers"));
+        this.wrapper.append(this.createSelect("gametypes"));
+        this.wrapper.append(this.createSelect("maps"));
+        this.wrapper.append(this.createSelect("display"));
+    }
+
 }
