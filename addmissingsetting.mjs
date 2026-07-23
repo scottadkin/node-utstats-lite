b27bd963e6496317f4dde2b23fba791b5356221f
Scott Adkin <scottadkin001@gmail.com>
2026-07-23 12:34:33 +0100

    create missing player settings

--- addmissingsetting.mjs
+++ addmissingsetting.mjs
@@ -1,3 +1,8 @@
 import { restoreDefaultSettings } from "./src/siteSettings.mjs";
+import { installPlayerSettings } from "./src/players.mjs";
 
 await restoreDefaultSettings();
+
+await installPlayerSettings();
+
+process.exit();
